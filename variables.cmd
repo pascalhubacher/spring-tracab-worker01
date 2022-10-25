82ede233e9464d24df7115f087844f4c152bdab7
Pascal Hubacher <pascal.hubacher@outlook.com>
2022-10-25 20:56:05 +0200

    add scripts to set variables

---
+++ variables.cmd
@@ -0,0 +1,4 @@
+set PUBLIC_IP=localhost
+set DOCKER_HOST_IP=localhost
+set DATAPLATFORM_HOME=localhost
+set DATAPLATFORM_IP=localhost
---
+++ variables.ps1
@@ -0,0 +1,8 @@
+$env:PUBLIC_IP = "localhost"
+$env:DOCKER_HOST_IP = "localhost"
+$env:DATAPLATFORM_HOME = "localhost"
+$env:DATAPLATFORM_IP = "localhost"
+Write-Output '$env:PUBLIC_IP = "localhost" is set'
+Write-Output '$end:DOCKER_HOST_IP = "localhost" is set'
+Write-Output '$end:DATAPLATFORM_HOME = "localhost" is set'
+Write-Output '$env:DATAPLATFORM_IP = "localhost"'
