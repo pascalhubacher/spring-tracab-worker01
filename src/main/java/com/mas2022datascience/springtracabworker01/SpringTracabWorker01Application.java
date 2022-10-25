6796c8af07e865dacaa240157121a0929ecfac3f
Pascal Hubacher <pascal.hubacher@outlook.com>
2022-10-25 21:13:31 +0200

    add spring initializr

---
+++ src/main/java/com/mas2022datascience/springtracabworker01/SpringTracabWorker01Application.java
@@ -0,0 +1,13 @@
+package com.mas2022datascience.springtracabworker01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTracabWorker01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTracabWorker01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mas2022datascience/springtracabworker01/SpringTracabWorker01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mas2022datascience.springtracabworker01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTracabWorker01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
