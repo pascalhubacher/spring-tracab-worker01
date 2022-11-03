dd7964482dfbb14c41e45e8564de7e402aaa5b46
Pascal Hubacher <pascal.hubacher@outlook.com>
2022-11-03 21:34:38 +0100

    complete singelton to calculate velocity and acceleration

--- src/main/java/com/mas2022datascience/springtracabworker01/processor/KafkaStreamsRunnerDSL.java
+++ src/main/java/com/mas2022datascience/springtracabworker01/processor/KafkaStreamsRunnerDSL.java
@@ -22,29 +22,9 @@ public class KafkaStreamsRunnerDSL {
 
     KStream<String, Frame> stream = kStreamBuilder.stream(topicIn);
 
-//    KTable<String, Frame> frame = stream.toTable(Materialized.as("test"));
-
-//    InMemoryKeyValueStore<String, Object> keyValueStore =
-//        streams.store("CountsKeyValueStore", QueryableStoreTypes.keyValueStore());
-
-//    KTable<String, Arrays<Integer>> objects = stream.
-
-//  <Frame utc="2019-06-05T18:47:25.843" isBallInPlay="1" ballPossession="Away">
-//    <Objs>
-//      <Obj type="7" id="0" x="4111" y="2942" z="11" sampling="0" />
-//      <Obj type="1" id="97746" x="4847" y="630" sampling="0" />
-//      <Obj type="1" id="250079677" x="1439" y="2441" sampling="0" />
-//  key: id
-//  value: [x, y, z]
-
-
     stream.mapValues(valueFrame ->
-            singletonList.getInstance().getVelocity(valueFrame))
+            singletonList.getInstance().calculate(valueFrame))
         .to(topicOut);
-    // publish to the output topic
-//    KStream<String, Frame> velocityStream = stream.
-    //KStream<Void, String> upperStream = stream.mapValues(value -> value.toUpperCase());
-    //upperStream.to(topicOut);
 
     return stream;
 
