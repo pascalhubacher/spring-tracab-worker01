29adfa822219d348ae8d2ff21f3c8e04f888f857
Pascal Hubacher <pascal.hubacher@outlook.com>
2022-11-04 13:27:14 +0100

    modify help to run worker

--- dockerize.cmd
+++ dockerize.cmd
@@ -1,3 +1,6 @@
 docker build -t spring-tracab-worker01:0.0.1 -t spring-tracab-worker01:latest .
 
-REM docker run --env DATAPLATFORM_IP=localhost -it -p 8080:8080 spring-tracab-worker01:latest
+REM if it runs on localhost
+REM docker run -it -p 8080:8080 spring-tracab-worker01:latest
+REM if it runs on 192.168.1.100
+REM docker run --env DATAPLATFORM_IP=192.168.1.100 -it -p 8080:8080 spring-tracab-worker01:latest
